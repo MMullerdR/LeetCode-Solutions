93153eb44c789437223a4ffdef0b5aec71252b52
MMullerdR <miguelmullerdarosa@gmail.com>
2026-03-30 08:42:55 -0300

    solution roman to integer problem

---
+++ arrays/romantointeger.c
@@ -0,0 +1,56 @@
+#include <string.h> // biblioteca adicionada pelo uso do strlen que dava warning
+
+int romanToInt(char* s) {
+    int length = strlen(s);
+    int sum = 0;
+
+    for(int i = 0; i < length; i++){
+        if(s[i] == 'I' && s[i+1] == 'V'){
+            sum = sum + 4; 
+            i++;
+        } 
+        else if(s[i] == 'I' && s[i+1] == 'X'){
+            sum = sum + 9;
+            i++;
+        }
+        else if(s[i] == 'X' && s[i+1] == 'L'){
+            sum = sum + 40; 
+            i++;
+        } 
+        else if(s[i] == 'X' && s[i+1] == 'C'){
+            sum = sum + 90;
+            i++;
+        }
+        else if(s[i] == 'C' && s[i+1] == 'D'){
+            sum = sum + 400; 
+            i++;
+        } 
+        else if(s[i] == 'C' && s[i+1] == 'M'){
+            sum = sum + 900;
+            i++;
+        }
+        else if(s[i] == 'I'){
+            sum = sum + 1;
+        }
+        else if(s[i] == 'V'){
+            sum = sum + 5;
+        }
+        else if(s[i] == 'X'){
+            sum = sum + 10;
+        }
+        else if(s[i] == 'L'){
+            sum = sum + 50;
+        }
+        else if(s[i] == 'C'){
+            sum = sum + 100;
+        }
+        else if(s[i] == 'D'){
+            sum = sum + 500;
+        }
+        else if(s[i] == 'M'){
+            sum = sum + 1000;
+        }
+        
+    }
+    return sum;
+}
