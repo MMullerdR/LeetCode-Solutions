1bfa05a66a63f835992f3c6fc9f57ce5739b56e8
MMullerdR <miguelmullerdarosa@gmail.com>
2026-05-17 00:32:59 -0300

    Readmes atualizados / 'main' implementada para rodar no valgrind / analise de complexidade

--- Entregas/trabalho-2/503.nextgreaterelementII.c
+++ Entregas/trabalho-2/503.nextgreaterelementII.c
@@ -5,29 +5,29 @@
 int* nextGreaterElements(int* nums, int numsSize, int* returnSize) {
     int *array = malloc(numsSize * sizeof(*array));
     
-    bool isFinded = false;
+    bool wasFound = false; 
 
     for ( int j = 0; j < numsSize; j++ ) {
         
         int actualNumber = nums[j];
 
-        for ( int i = j + 1; i < numsSize; i++ ) {
+        for ( int i = j + 1; i < numsSize; i++ ) { // busca nos numeros que vem depois do numero atual
 
             if ( actualNumber < nums[i] ) { 
                 array[j] = nums[i];
-                isFinded = true;
+                wasFound = true;
                 break;
             }
             
         }
 
-        if ( isFinded == false ){
+        if ( wasFound == false ){  // busca nos numeros que vem antes do numero atual
 
             for ( int i = 0; i < j; i++ ) { 
 
                 if ( actualNumber < nums[i] ) { 
                     array[j] = nums[i];
-                    isFinded = true;
+                    wasFound = true;
                     break;
                 }
             
@@ -35,11 +35,11 @@ int* nextGreaterElements(int* nums, int numsSize, int* returnSize) {
 
         }
 
-        if ( isFinded == false ) {
+        if ( wasFound == false ) { // nao encontrou
             array[j] = -1;
         }
 
-        isFinded = false;
+        wasFound = false;
         
     }
 
